4fb6213b0d4694815083ae9af57f8b6ca686e9c4
ballinyouup <bryanhuici@gmail.com>
2024-06-09 14:31:54 -0400

    moved to .env file with dotenv

--- src/main/java/com/insurenebula/crm/Application.java
+++ src/main/java/com/insurenebula/crm/Application.java
@@ -3,10 +3,19 @@ package com.insurenebula.crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class Application {
-    // private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        // Set environment variables
+        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("DATABASE_URL"));
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+
         SpringApplication.run(Application.class, args);
     }
 
